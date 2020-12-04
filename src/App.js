2b14316cf099f45a0d649bde8ddfc25d9dbea050
bruce <bruce.wang@zerologix.com>
2020-12-04 20:30:13 +1100

    update current projects

--- src/App.js
+++ src/App.js
@@ -9,184 +9,194 @@ var storage = window.localStorage;
 const DEFAULT = "default_list";
 
 const getSelectedNumber =(list,calcuPower)=>{
-  const archDegree = 360/list.length
+    const archDegree = 360/list.length
     const rotationAngle = map(calcuPower, 0, 100, 0, 1700)
     const balance = rotationAngle%360;
     const arrowLocation =(Math.abs(360-balance)-90)/archDegree
     const numberIndex = arrowLocation>0?Math.ceil(arrowLocation):Math.ceil(list.length+arrowLocation)
     return list[numberIndex-1];
-
 }
 
 const default_list = JSON.parse(storage.getItem(DEFAULT)) ?? [
-  "金鳳",
-  "金坊",
-  "叁茶陸飯",
-  "意樂",
-  "韓閣",
+    "金鳳",
+    "金坊",
+    "叁茶陸飯",
+    "意樂",
+    "韓閣",
 ];
 
 // const OFFSET = Math.random();
 const OFFSET =0;
 
 const map = function (value, in_min, in_max, out_min, out_max) {
-  if (value === 0) {
-    return out_min;
-  }
-  return ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+    if (value === 0) {
+        return out_min;
+    }
+    return ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
 
 function App() {
     const [winnerList,setWinnerList] =useState([]);
-  const r = 190;
-  const cx = 250;
-  const cy = 250;
-  const [list, setList] = useState(default_list);
-  const [name, setName] = useState("");
-  const [power, setPower] = useState(0);
-  const [previousPower,setPreviousPower]= useState(0)
-  const [acc, setAcc] = useState(0);
-  const config = { mass: 50, tension: 200, friction: 200, precision: 0.001 };
-  const [props, set] = useSpring(() => ({
-    transform: "rotate(0deg)",
-    immediate: false,
-  }));
-  const addItem = () => {
-    storage.setItem(DEFAULT, JSON.stringify([...list, name]));
-    setList([...list, name]);
-    setName("");
-  };
-  const deleteItem = (e) => {
-    const { item } = e.currentTarget.dataset;
-    storage.setItem(DEFAULT, JSON.stringify(list.filter((e) => e !== item)));
-    setList(list.filter((e) => e !== item));
-  };
-  const reset=()=>{
-    storage.clear()
-    window.location.reload()
-  }
+    const r = 190;
+    const cx = 250;
+    const cy = 250;
+    const [list, setList] = useState(default_list);
+    const [name, setName] = useState("");
+    const [power, setPower] = useState(0);
+    const [previousPower,setPreviousPower]= useState(0)
+    const [acc, setAcc] = useState(0);
+    const config = { mass: 50, tension: 200, friction: 200, precision: 0.001 ,duration:500};
+    const [props, set] = useSpring(() => ({
+        transform: "rotate(0deg)",
+        immediate: false,
+    }));
+    const addItem = () => {
+        storage.setItem(DEFAULT, JSON.stringify([...list, name]));
+        setList([...list, name]);
+        setName("");
+    };
+    const deleteItem = (e) => {
+        const { item } = e.currentTarget.dataset;
+        storage.setItem(DEFAULT, JSON.stringify(list.filter((e) => e !== item)));
+        setList(list.filter((e) => e !== item));
+    };
+    const reset=()=>{
+        storage.clear()
+        window.location.reload()
+    }
 
-  useEffect(() => {
-      if(power!==previousPower){
-          set({
-              from: { transform: `rotate(${map(acc, 0, 100, 0, 1700)}deg)` },
-              transform: `rotate(${map(acc + power, 0, 100, 0, 1700)}deg)`,
-              immediate: false,
-              config,
-          });
-          setAcc(acc + power);
-          setPreviousPower(power);
-          const calcuPower =acc+power
-          const selectedNumber =getSelectedNumber(list,calcuPower)
-         //TODO add a popups which enable to create a button and user may click button to remove privious result 
-         // setList(list.filter((e) => e !== selectedNumber));
+    useEffect(() => {
+        if(power!==previousPower){
+            const calcuPower =acc+power
+            set({
+                from: { transform: `rotate(${map(acc, 0, 100, 0, 1700)}deg)` },
+                transform: `rotate(${map(acc + power, 0, 100, 0, 1700)}deg)`,
+                immediate: false,
+                config,
+            });
+            setAcc(calcuPower);
+            setPreviousPower(power);
 
-          if(!winnerList.includes(selectedNumber)){
-              setWinnerList([...winnerList,selectedNumber])
-          }
-      }
-  }, [acc, config, list, power, previousPower, set, winnerList]);
+            const selectedNumber =getSelectedNumber(list,calcuPower)
+
+            //TODO add a popups which enable to create a button and user may click button to remove privious result 
+            //
+            setTimeout(()=>{
+                if(!winnerList.includes(selectedNumber)){
+                    setWinnerList([...winnerList,selectedNumber])
+                    //TODO show the result tab popups
+                    setList(list.filter((e) => e !== selectedNumber));
+                }
+
+            },600)
+
+
+//             if(!winnerList.includes(selectedNumber)){
+//                 setWinnerList([...winnerList,selectedNumber])
+//             }
+        }
+    }, [acc, config, list, power, previousPower, set, winnerList]);
 
 
 
-  const rederItems = (numOfItems) => {
-    let items = [];
-    for (let i = 0; i < numOfItems; i++) {
-      let xLength = Math.cos(2 * Math.PI * (i / numOfItems + OFFSET)) * (r - 5);
-      let yLength = Math.sin(2 * Math.PI * (i / numOfItems + OFFSET)) * (r - 5);
-      let txLength =
-        Math.cos(2 * Math.PI * ((i + 0.5) / numOfItems + OFFSET)) * (r / 2);
-      let tyLength =
-        Math.sin(2 * Math.PI * ((i + 0.5) / numOfItems + OFFSET)) * (r / 2);
-      items.push(
-        <Fragment key={i}>
-          <line
-            stroke="#4065a2"
-            strokeWidth="2"
-            x1={cx + xLength}
-            y1={cy + yLength}
-            x2={cx}
-            y2={cy}
-          />
-          <text
-            x={cx + txLength}
-            y={cy + tyLength}
-            fontSize="15px"
-            transform={`rotate(${((i + 0.5) / numOfItems + OFFSET) * 360}
+    const rederItems = (numOfItems) => {
+        let items = [];
+        for (let i = 0; i < numOfItems; i++) {
+            let xLength = Math.cos(2 * Math.PI * (i / numOfItems + OFFSET)) * (r - 5);
+            let yLength = Math.sin(2 * Math.PI * (i / numOfItems + OFFSET)) * (r - 5);
+            let txLength =
+                Math.cos(2 * Math.PI * ((i + 0.5) / numOfItems + OFFSET)) * (r / 2);
+            let tyLength =
+                Math.sin(2 * Math.PI * ((i + 0.5) / numOfItems + OFFSET)) * (r / 2);
+            items.push(
+                <Fragment key={i}>
+                    <line
+                        stroke="#4065a2"
+                        strokeWidth="2"
+                        x1={cx + xLength}
+                        y1={cy + yLength}
+                        x2={cx}
+                        y2={cy}
+                    />
+                            <text
+                                x={cx + txLength}
+                                y={cy + tyLength}
+                                fontSize="15px"
+                                transform={`rotate(${((i + 0.5) / numOfItems + OFFSET) * 360}
                   ${cx + txLength},
                   ${cy + tyLength})`}
-          >
-            {list[i]}
-          </text>
-        </Fragment>
-      );
-    }
-    return items;
-  };
+                            >
+                                    {list[i]}
+                                </text>
+                                    </Fragment>
+            );
+        }
+        return items;
+    };
 
 
-  return (
-    <div className="main-wrapper" style={{ overflowX: "hidden" }}>
-        <div className="content-wrapper">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 500 500"
-        style={{ width: "60vw", height: "80vh" }}
-      >
-        <g fill="white" stroke="green" strokeWidth="10">
-          <circle cx="250" cy="250" r={r} />
-        </g>
-        <animated.g
-          style={{
-            transform: props.transform,
-            transformOrigin: "center",
-          }}
-        >
-          {rederItems(list.length)}
-        </animated.g>
-        <g fill="#7fdaf8">
-          <circle cx="250" cy="250" r="15" />
-        </g>
-        <g fill="black">
-          <circle cx="250" cy="250" r="4" />
-        </g>
-        <g fill="#1a3997" stroke="#91abd0" strokeWidth="2">
-          <polygon points="250,70 230,10 270,10" />
-        </g>
-      </svg>
-      <PressButton setPower={setPower} />
-      <div style={{ marginTop: "20vh", marginBottom: "5vh" }}>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        <button className="button" onClick={addItem}>Add</button>
-        <button className="button" onClick={reset}>reset</button>
-        {list.map(n => (
-          <div key={n} className="item">
-            {n}
-            <Logo
-              data-item={n}
-              fill="#a3aab8"
-              style={{ height: "1em", width:'auto', verticalAlign: "sub", marginLeft: "5px" }}
-              onClick={deleteItem}
-            />
-          </div>
-        ))}
-      </div>
-          </div>
-        <Winners winnerList={winnerList}/>
-    </div>
-  );
+    return (
+        <div className="main-wrapper" style={{ overflowX: "hidden" }}>
+            <div className="content-wrapper">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 500 500"
+                    style={{ width: "60vw", height: "80vh" }}
+                >
+                        <g fill="white" stroke="green" strokeWidth="10">
+                            <circle cx="250" cy="250" r={r} />
+                                </g>
+                                    <animated.g
+                                        style={{
+                                            transform: props.transform,
+                                            transformOrigin: "center",
+                                        }}
+                                    >
+                                            {rederItems(list.length)}
+                                        </animated.g>
+                                            <g fill="#7fdaf8">
+                                                <circle cx="250" cy="250" r="15" />
+                                                    </g>
+                                                        <g fill="black">
+                                                            <circle cx="250" cy="250" r="4" />
+                                                                </g>
+                                                                    <g fill="#1a3997" stroke="#91abd0" strokeWidth="2">
+                                                                        <polygon points="250,70 230,10 270,10" />
+                                                                            </g>
+                                                                                </svg>
+                                                                                    <PressButton setPower={setPower} />
+                                                                                        <div style={{ marginTop: "20vh", marginBottom: "5vh" }}>
+                                                                                            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                                                                                                <button className="button" onClick={addItem}>Add</button>
+                                                                                                    <button className="button" onClick={reset}>reset</button>
+                                                                                                        {list.map(n => (
+                                                                                                            <div key={n} className="item">
+                                                                                                                {n}
+                                                                                                                <Logo
+                                                                                                                    data-item={n}
+                                                                                                                    fill="#a3aab8"
+                                                                                                                    style={{ height: "1em", width:'auto', verticalAlign: "sub", marginLeft: "5px" }}
+                                                                                                                    onClick={deleteItem}
+                                                                                                                />
+                                                                                                                        </div>
+                                                                                                        ))}
+                                                                                                                            </div>
+                                                                                                                                </div>
+                                                                                                                                    <Winners winnerList={winnerList}/>
+                                                                                                                                        </div>
+    );
 }
 
 const PressButton = ({ setPower }) => {
     const randomPressure = Math.random()*100
     console.log('randomPressure',randomPressure)
-  return (
-    <button
-      className="main"
-        onClick={()=>{setPower(randomPressure)}}
-    >
-     Start Rolling </button>
-  );
+    return (
+        <button
+            className="main"
+            onClick={()=>{setPower(randomPressure)}}
+        >
+                Start Rolling </button>
+    );
 };
 
 export default App;
--- src/winnerList/WinnerList.jsx
+++ src/winnerList/WinnerList.jsx
@@ -4,7 +4,7 @@ import './winnerList.css'
 const WinnerList = ({winnerList=[]}) => {
     return (
         <div className='winnerList'style={{color:"#fff"}}>
-            <h1>Winner List :</h1>
+            <h1>Winners List :</h1>
                 {winnerList.length>0?(<>
                     {winnerList.map((winner,index)=>{
                         return<div className='winner'><span className='order'>{index+1}</span><span>{winner}</span></div>
