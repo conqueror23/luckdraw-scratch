7ce96a3e968055afef0e6c80d9837b34e9b88ac9
bruce <bruce.wang@zerologix.com>
2020-12-05 19:52:44 +1100

    Updates

---
+++ precache-manifest.33428030594861785884f3322ecc56cd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf425fb8f30021edf1df0f4348fa0bd",
+    "url": "/lucky-wheel/index.html"
+  },
+  {
+    "revision": "edaa6ca84ca112465ae8",
+    "url": "/lucky-wheel/static/css/main.2db42355.chunk.css"
+  },
+  {
+    "revision": "c38a5bdb9e3234f98b2f",
+    "url": "/lucky-wheel/static/js/2.fef2e093.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lucky-wheel/static/js/2.fef2e093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edaa6ca84ca112465ae8",
+    "url": "/lucky-wheel/static/js/main.53beebe7.chunk.js"
+  },
+  {
+    "revision": "a7672e4aa77cd5f7a4e4",
+    "url": "/lucky-wheel/static/js/runtime-main.ab7cd794.js"
+  },
+  {
+    "revision": "c5820fbb3c481ae120dcf0082224831a",
+    "url": "/lucky-wheel/static/media/acy_security_logo.c5820fbb.png"
+  },
+  {
+    "revision": "5ace3fb806a8ad01ce8011077068b304",
+    "url": "/lucky-wheel/static/media/delete.5ace3fb8.svg"
+  },
+  {
+    "revision": "b9e77b8a31c5f23fb3674b2b983f2e81",
+    "url": "/lucky-wheel/static/media/santax.b9e77b8a.gif"
+  },
+  {
+    "revision": "abaf2efe8981688cf83d76380d232c26",
+    "url": "/lucky-wheel/static/media/tree.abaf2efe.gif"
+  }
+]);
